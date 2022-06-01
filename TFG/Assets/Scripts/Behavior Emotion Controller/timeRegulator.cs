90fcc0d94db4fc08f820ec6bd2fa577b381a9747
hatercillo <79996838+hatercillo@users.noreply.github.com>
2022-06-01 14:47:44 +0200

    Add files via upload

---
+++ TFG/Assets/Scripts/Behavior Emotion Controller/timeRegulator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeRegulator : MonoBehaviour
+{
+    private emotion em;
+    public int time;
+    public bool asc;
+    private int counter;
+//    public EmotionalController controller;
+    public int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        em = this.GetComponent<emotion>();
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter == time)
+        {
+            
+            if (asc)
+            {
+                if (em.valor<0.5)
+                {
+                    em.incmin();
+                }
+                else
+                {
+                    em.decmin();
+                }
+            }
+            else
+            {
+                em.decmin();
+            }
+            counter = 0;
+        }
+        
+    }
+}
