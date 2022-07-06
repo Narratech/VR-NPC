18b955068593368ae91d076603294da0331f3e8a
hatercillo <79996838+hatercillo@users.noreply.github.com>
2022-07-06 18:58:26 +0200

    Add files via upload

---
+++ TFG Rafael Marquez/Assets/Scripts/Behavior Emotion Controller/RobotTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RobotTest : MonoBehaviour
+{
+    public static int arbol;
+    public static double angermultiplier = 1;
+    public static double surprisemultiplier = 1;
+    public static double sadmultiplier = 1;
+    public static double ascomultiplier = 1;
+    public static double happymultiplier = 1;
+    public static double fearmultiplier = 1;
+    public static double disgustmultiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void load(double an, double hap, double sad,int value)
+    {
+        arbol = value;
+        angermultiplier = an;
+        surprisemultiplier = 1;
+        ascomultiplier =1 ;
+        sadmultiplier = sad;
+        happymultiplier = hap;
+        fearmultiplier = 1;
+        disgustmultiplier = 1;
+        SceneManager.LoadScene("EscapeRoomPrueba");
+    }
+}
---
+++ TFG Rafael Marquez/Assets/Scripts/Behavior Emotion Controller/multiplierselector.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+namespace BehaviorEmotion
+{
+    [TaskCategory("BehaviorEmotion/Selectors")]
+    public class multiplierselector : Composite
+    {
+
+        private GameObject selector;
+        private EmotionalController control;
+        private int currentChildIndex = 0;
+        // The task status of the last child ran.
+        private TaskStatus executionStatus = TaskStatus.Inactive;
+        public override void OnStart()
+        {
+            selector = GameObject.Find("EmotionalSistem");
+            control = selector.GetComponent<EmotionalController>();
+            currentChildIndex = Highestmotion();
+        }
+
+        public override int CurrentChildIndex()
+        {
+            return Highestmotion();
+        }
+
+        public override bool CanExecute()
+        {
+
+            // We can continue to execuate as long as we have children that haven't been executed and no child has returned failure.
+            //return currentChildIndex < children.Count && executionStatus != TaskStatus.Failure;
+            return currentChildIndex == Highestmotion() && executionStatus != TaskStatus.Failure;
+        }
+
+        public override void OnChildExecuted(TaskStatus childStatus)
+        {
+            // Increase the child index and update the execution status after a child has finished running.
+            currentChildIndex++;
+            executionStatus = childStatus;
+        }
+
+        public override void OnConditionalAbort(int childIndex)
+        {
+            // Set the current child index to the index that caused the abort
+            currentChildIndex = Highestmotion();
+            executionStatus = TaskStatus.Inactive;
+        }
+
+        public override void OnEnd()
+        {
+            // All of the children have run. Reset the variables back to their starting values.
+            executionStatus = TaskStatus.Inactive;
+            currentChildIndex = Highestmotion();
+        }
+        public int Highestmotion()
+        {
+            int index = 0;
+            double mayor = 0;
+            for (int i = 0; i < this.control.conjunto_emocional.Length; i++)
+            {
+                if (this.control.conjunto_emocional[i].multiplicador > mayor)
+                {
+                    index = i;
+                    mayor = this.control.conjunto_emocional[i].multiplicador;
+                }
+            }
+            return index;
+        }
+    }
+
+}
